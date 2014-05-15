4a951b0d505ed0f3f3de4bf612f0b78e63941769
glf12 <glf12@PERINATAL096-LAP.gipl>
2014-05-15 16:32:41 +0100

    updated distribution url

--- pynii/src/setup.py
+++ pynii/src/setup.py
@@ -4,13 +4,12 @@ Created on 15 May 2014
 @author: glf12
 '''
 from distutils.core import setup
-import os
 
 setup(name='pynii',
       version='0.9',
       description='A simple python nifti interface derived from nibabel',
       author='Gianlorenzo Fagiolo',
-      url='https://github.com/gianlo',
+      url='https://github.com/gianlo/pynii',
       license='License :: OSI Approved :: MIT License',
       requires= ['numpy'],
       py_modules=['pynii'])
